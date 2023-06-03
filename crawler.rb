05afcc78a828eb575cd6f7ae391bf2c8655b0747
Andrei Kislichenko <android.2net@gmail.com>
2023-06-03 11:45:02 -0400

    add index

--- crawler.rb
+++ crawler.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
-require "fileutils"
+require 'fileutils'
+require 'relaton/index'
 
 t1 = Time.now
 puts "Started at: #{t1}"
 
-system("git clone https://github.com/ietf-tools/relaton-data-ieee")
-FileUtils.rm_rf("data")
-FileUtils.cp_r("relaton-data-ieee/data", ".")
-FileUtils.rm_rf("relaton-data-ieee")
+system('git clone https://github.com/ietf-tools/relaton-data-ieee')
+FileUtils.rm_rf('data')
+FileUtils.cp_r('relaton-data-ieee/data', '.')
+FileUtils.rm_rf('relaton-data-ieee')
+
+index = Relaton::Index.find_or_create :ieee, file: 'index-v1.yaml'
+Dir['data/*.yaml'].each do |f|
+  yaml = YAML.load_file f
+  docid = yaml['docid'].find { |i| i['type'] == 'IEEE' && i['primary'] == true && i['trademark'].nil? }
+  index.add_or_update docid['id'], f
+end
+index.save
 
 t2 = Time.now
 puts "Stopped at: #{t2}"
