2f50de128917f16a067a0c33649b9ebce2dc16ea
Andrei Kislichenko <android.2net@gmail.com>
2025-08-31 09:39:17 -0400

    fix issue metanorma/metanorma-ieee#531

--- crawler.rb
+++ crawler.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-require 'fileutils'
-require 'relaton/index'
+# @TODO: uncomment everithing below when https://github.com/ietf-tools/relaton-data-ieee is updated
+
+# require 'fileutils'
+# require 'relaton/index'
 
 t1 = Time.now
 puts "Started at: #{t1}"
 
-system('git clone https://github.com/ietf-tools/relaton-data-ieee')
-FileUtils.rm_rf('data')
-FileUtils.cp_r('relaton-data-ieee/data', '.')
-FileUtils.rm_rf('relaton-data-ieee')
+# system('git clone https://github.com/ietf-tools/relaton-data-ieee')
+# FileUtils.rm_rf('data')
+# FileUtils.cp_r('relaton-data-ieee/data', '.')
+# FileUtils.rm_rf('relaton-data-ieee')
 
-FileUtils.rm Dir.glob('index*')
-index = Relaton::Index.find_or_create :ieee, file: 'index-v1.yaml'
-Dir['data/*.yaml'].each do |f|
-  yaml = YAML.load_file f
-  docid = yaml['docid'].find { |i| i['type'] == 'IEEE' && i['primary'] == true && i['trademark'].nil? }
-  index.add_or_update docid['id'], f
-end
-index.save
+# FileUtils.rm Dir.glob('index*')
+# index = Relaton::Index.find_or_create :ieee, file: 'index-v1.yaml'
+# Dir['data/*.yaml'].each do |f|
+#   yaml = YAML.load_file f
+#   docid = yaml['docid'].find { |i| i['type'] == 'IEEE' && i['primary'] == true && i['trademark'].nil? }
+#   index.add_or_update docid['id'], f
+# end
+# index.save
 
 t2 = Time.now
 puts "Stopped at: #{t2}"
